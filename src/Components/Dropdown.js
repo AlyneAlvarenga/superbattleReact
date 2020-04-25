fa06a5eb7db919bac306a465b47922dc83694141
AlyneAlvarenga <alynevs@gmail.com>
2020-04-25 18:45:32 -0400

    trying to make promise.all work with useeffect

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -3,52 +3,56 @@ import axios from 'axios';
 
 const Dropdown = () => {
 
-  const superheroIds = [18, 30, 35, 38, 61, 69, 95, 106, 107, 127];
+  const superheroIds = [18, 30, 35, 38];
 
   const [superheroData, setSuperheroData] = useState([]);
-  
-  // , 149, 157, 162, 176, 208, 213, 226, 275, 278, 289, 303, 313, 332, 346, 370, 374, 405, 410, 414, 418, 423, 487
 
-  const getAllSuperheroes = (idArr) => {
-    return Promise.all(idArr.map(id => {
-      return axios({
-        method: 'GET',
-        url: 'https://proxy.hackeryou.com',
-        responseType: 'json',
-        params: {
-          reqUrl: `https://superheroapi.com/api/2827401063977440/${id}`,
-          proxyHeaders: {
-            'header_params': 'value'
-          },
-          xmlToJSON: false,
-        }
-      }).then(response => {
-        console.log(response.data);
-        
-      })
-    }))
-  }
+  // const getAllSuperheroes = (idArr) => {
+  //   return Promise.all(idArr.map(id => {
+  //     return axios({
+  //       method: 'GET',
+  //       url: 'https://proxy.hackeryou.com',
+  //       responseType: 'json',
+  //       params: {
+  //         reqUrl: `https://superheroapi.com/api/2827401063977440/${id}`,
+  //         proxyHeaders: {
+  //           'header_params': 'value'
+  //         },
+  //         xmlToJSON: false,
+  //       }
+  //     }).then(response => {
+  //       console.log(response.data);
+  //     })
+  //   }))
+  // }
+
+  
 
   useEffect(() => {
+    const getAllSuperheroes = (idArr) => {
+      return idArr.map(id => {
+        return axios({
+          method: 'GET',
+          url: 'https://proxy.hackeryou.com',
+          responseType: 'json',
+          params: {
+            reqUrl: `https://superheroapi.com/api/2827401063977440/${id}`,
+            proxyHeaders: {
+              'header_params': 'value'
+            },
+            xmlToJSON: false,
+          }
+        })
+      })
+    }
 
-    getAllSuperheroes(superheroIds);
-    // const getData = async () => {
-    //   const response = await axios({
-    //     method: 'GET',
-    //     url: 'https://proxy.hackeryou.com',
-    //     responseType: 'json',
-    //     params: {
-    //       reqUrl: `https://superheroapi.com/api/2827401063977440/24`,
-    //       proxyHeaders: {
-    //         'header_params': 'value'
-    //       },
-    //       xmlToJSON: false,
-    //     }
-    //   });
-    //   console.log(response.data);
-    // }
-    // getData();
-  }, [superheroIds])
+    const promises = getAllSuperheroes(superheroIds);
+    const objects = Promise.allSettled(promises).then(res => res.data);
+    console.log(objects);
+    
+    // setSuperheroData(objects);
+
+  }, [superheroIds]); 
 
   return (
     <form action="">
