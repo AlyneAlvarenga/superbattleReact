af7c1bc0a3386dcc471d6b78968a05945d39fd63
AlyneAlvarenga <alynevs@gmail.com>
2020-04-27 15:51:03 -0400

    calculated the winner, displayed img and number

--- src/Components/BattleCondition.js
+++ src/Components/BattleCondition.js
@@ -5,9 +5,10 @@ const BattleCondition = ({handleChange}) => {
     <select name="" id="" onChange={handleChange} >
       <option value="">Select the Battle Condition</option>
       <option value="intelligence">Battle of Intelligence</option>
-      <option value="speed">Battle of speed</option>
-      <option value="combat">Battle of combat</option>
-      <option value="strength">Battle of strength</option>
+      <option value="speed">Battle of Speed</option>
+      <option value="combat">Battle of Combat</option>
+      <option value="strength">Battle of Strength</option>
+      <option value="power">Battle of Power</option>
     </select>
   )
 }
--- src/Components/Winner.js
+++ src/Components/Winner.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Winner = ({superhero1, superhero2, chosenBattle}) => {
+  // const [superhero1Total, setSuperhero1Total] = useState(0);
+  // const [superhero2Total, setSuperhero2Total] = useState(0);
+
+  const whoIsWinner = () => {
+    const hero1 = superhero1[0].powerstats[chosenBattle];
+    const hero1Int = parseInt(hero1);
+
+    const hero2 = superhero2[0].powerstats[chosenBattle];
+    const hero2Int = parseInt(hero2);
+
+    if (hero1Int > hero2Int) {
+      return superhero1[0].name;
+    } else if (hero1Int < hero2Int) {
+      return superhero2[0].name;
+    } else if (hero1Int === hero2Int) {
+      return `Both! It's a tie!`;
+    }
+  }
+
   return (
     <section>
-      <h2>The winner of the battle of {chosenBattle} is...</h2>
-      
+      <h2>The winner of the battle of {chosenBattle} is {whoIsWinner()}</h2>
+      <div className="Winner-resultContainer">
+        <div className="Winner-imgContainer">
+          <img src={superhero1[0].image.url} alt={superhero1[0].name} className={ whoIsWinner() === superhero1[0].name ? null : 'grayscale' } />
+        </div>
+        <div>{superhero1[0].powerstats[chosenBattle]}</div>
+      </div>
+      <div className="Winner-resultContainer">
+        <div className="Winner-imgContainer">
+          <img src={superhero2[0].image.url} alt={superhero2[0].name} className={whoIsWinner() === superhero2[0].name ? null : 'grayscale'} />
+        </div>
+        <div>{superhero2[0].powerstats[chosenBattle]}</div>
+      </div>
     </section>
   )
 }
