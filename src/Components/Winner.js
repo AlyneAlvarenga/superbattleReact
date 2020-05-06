d207c574f3c1964f9891bcb2a1d20453a71dd3d8
AlyneAlvarenga <alynevs@gmail.com>
2020-05-06 15:50:24 -0400

    started implementing circles

--- src/Components/Winner.js
+++ src/Components/Winner.js
@@ -1,8 +1,17 @@
 import React, {useState} from 'react';
 
 const Winner = ({superhero1, superhero2, chosenBattle}) => {
-  // const [superhero1Total, setSuperhero1Total] = useState(0);
+  const [superhero1Percent, setSuperhero1Percent] = useState(0);
+  const [superhero2Percent, setSuperhero2Percent] = useState(0);
   // const [superhero2Total, setSuperhero2Total] = useState(0);
+
+  const drawCircles = (num1, num2) => {
+    const percentageHero1 = (num1 * 283 / 100).toFixed(0);
+    const percentageHero2 = (num2 * 283 / 100).toFixed(0);
+
+    setSuperhero1Percent(percentageHero1);
+    setSuperhero2Percent(percentageHero2);
+  }
 
   const whoIsWinner = () => {
     const hero1 = superhero1[0].powerstats[chosenBattle];
@@ -10,6 +19,8 @@ const Winner = ({superhero1, superhero2, chosenBattle}) => {
 
     const hero2 = superhero2[0].powerstats[chosenBattle];
     const hero2Int = parseInt(hero2);
+
+    drawCircles(hero1Int, hero2Int);
 
     if (hero1Int > hero2Int) {
       return superhero1[0].name;
@@ -27,13 +38,37 @@ const Winner = ({superhero1, superhero2, chosenBattle}) => {
         <div className="Winner-imgContainer">
           <img src={superhero1[0].image.url} alt={superhero1[0].name} className={ whoIsWinner() === superhero1[0].name ? null : 'grayscale' } />
         </div>
-        <div>{superhero1[0].powerstats[chosenBattle]}</div>
+        <div className="Winner-circleContainer">
+          <svg class="svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <g class="svgGroup">
+              <circle class="baseCircle" cx="50" cy="50" r="45" />
+              <path class="secondCircleHero1" stroke-dasharray={} d="
+                  M 50, 50
+                  m -45, 0
+                  a 45,45 0 1,0 90,0
+                  a 45,45 0 1,0 -90,0"></path>
+            </g>
+          </svg>
+          <span class="circleLabel circleLabelHero1">{superhero1[0].powerstats[chosenBattle]}</span>
+        </div>
       </div>
       <div className="Winner-resultContainer">
         <div className="Winner-imgContainer">
           <img src={superhero2[0].image.url} alt={superhero2[0].name} className={whoIsWinner() === superhero2[0].name ? null : 'grayscale'} />
         </div>
-        <div>{superhero2[0].powerstats[chosenBattle]}</div>
+        <div className="Winner-circleContainer">
+          <svg class="svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <g class="svgGroup">
+              <circle class="baseCircle" cx="50" cy="50" r="45" />
+              <path class="secondCircleHero2" d="
+                  M 50, 50
+                  m -45, 0
+                  a 45,45 0 1,0 90,0
+                  a 45,45 0 1,0 -90,0"></path>
+            </g>
+          </svg>
+          <span class="circleLabel circleLabelHero2">{superhero2[0].powerstats[chosenBattle]}</span>
+        </div>
       </div>
     </section>
   )
