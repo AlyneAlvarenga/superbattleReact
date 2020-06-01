01d0eaecdb47acb7a2991a863b792e9a4a5a078a
AlyneAlvarenga <alynevs@gmail.com>
2020-06-01 15:06:35 -0400

    Updates

---
+++ precache-manifest.aace0cf74fa2d17136cc5d0c8552edd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f67440632447432179750dd469f4cc7",
+    "url": "/superbattleReact/index.html"
+  },
+  {
+    "revision": "efb0b281f1535ec1eba4",
+    "url": "/superbattleReact/static/css/main.a146c42b.chunk.css"
+  },
+  {
+    "revision": "9f281beb03be2cff7bfe",
+    "url": "/superbattleReact/static/js/2.b28bd36e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/superbattleReact/static/js/2.b28bd36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb0b281f1535ec1eba4",
+    "url": "/superbattleReact/static/js/main.455bc60b.chunk.js"
+  },
+  {
+    "revision": "5e7ae31da8ab4ebee014",
+    "url": "/superbattleReact/static/js/runtime-main.060431c0.js"
+  }
+]);
